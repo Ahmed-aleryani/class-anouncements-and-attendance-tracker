2a4ea9d2f3989b601ba99426ff60cfa16b71a21e
Ahmed Aleryani <aalavyani91@yahoo.com>
2021-01-31 20:06:59 +0300

    Style: Edit home page background and add overlay
    Change Home page background to cover whole screen and added an overlay to fix distortion issue

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -7,8 +7,11 @@ const Home=(Props)=>{
     return(
         <div style={{backgroundImage:`url(${HomeImg})`,
             backgroundAttachment:'fixed'
-        }} className="h-auto md:h-screen lg:h-screen xl:h-screen flex flex-wrap justify-center">
-       
+        }} className="relative h-auto md:h-screen lg:h-screen xl:h-screen flex flex-wrap justify-center bg-cover">
+        <div className="absolute h-auto w-full md:h-screen lg:h-screen xl:h-screen bg-secondary-200 bg-opacity-50">
+
+        </div>
+        <div className="relative">
         <div className="mt-12 flex flex-col">
         <h1 className="text-6xl text-secondary-100 bg-transparent rounded-3xl p-10 ">Welcome to CAAT</h1>
         <p className="text-secondary-100 text-xl text-center">The place of all announcements :D</p>
@@ -17,6 +20,7 @@ const Home=(Props)=>{
            </Link> 
         </div>
 
+        </div>
         </div>
     )
 }
